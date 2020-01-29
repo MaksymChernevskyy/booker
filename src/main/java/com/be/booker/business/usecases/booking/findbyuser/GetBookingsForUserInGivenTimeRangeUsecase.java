0304fde7da5e54e18ac51c110dd17b791fbc7184
admin <chernevskyymaks@gmail.com>
2020-01-29 14:25:17 +0100

    Update service

--- src/main/java/com/be/booker/business/usecases/booking/findbyuser/GetBookingsForUserInGivenTimeRangeUsecase.java
+++ src/main/java/com/be/booker/business/usecases/booking/findbyuser/GetBookingsForUserInGivenTimeRangeUsecase.java
@@ -5,74 +5,69 @@ import com.be.booker.business.entity.entitydto.RoomBookingNameAndSurnameDto;
 import com.be.booker.business.exceptions.BadRequestException;
 import com.be.booker.business.repository.BookingRepository;
 import com.be.booker.business.repository.UserRepository;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.be.booker.business.usecases.booking.DateChecker;
 import com.be.booker.business.usecases.booking.MapBookings;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class GetBookingsForUserInGivenTimeRangeUsecase {
-  private BookingRepository bookingRepository;
-  private UserRepository userRepository;
-  private String userLogin;
-  private LocalDateTime bookedFrom;
-  private LocalDateTime bookedTo;
-  private MapBookings mapBookings;
-  private DateChecker dateChecker;
+    private BookingRepository bookingRepository;
+    private UserRepository userRepository;
+    private String userLogin;
+    private LocalDateTime bookedFrom;
+    private LocalDateTime bookedTo;
+    private MapBookings mapBookings;
+    private DateChecker dateChecker;
 
-  public GetBookingsForUserInGivenTimeRangeUsecase withBookingRepository(BookingRepository bookingRepository) {
-    this.bookingRepository = bookingRepository;
-    return this;
-  }
-
-  public GetBookingsForUserInGivenTimeRangeUsecase withUserRepository(UserRepository userRepository) {
-    this.userRepository = userRepository;
-    return this;
-  }
-
-  public GetBookingsForUserInGivenTimeRangeUsecase withBookedFrom(LocalDateTime bookedFrom) {
-    this.bookedFrom = bookedFrom;
-    return this;
-  }
-
-  public GetBookingsForUserInGivenTimeRangeUsecase withBookedTo(LocalDateTime bookedTo) {
-    this.bookedTo = bookedTo;
-    return this;
-  }
-
-  public GetBookingsForUserInGivenTimeRangeUsecase withUserLogin(String userLogin) {
-    this.userLogin = userLogin;
-    return this;
-  }
-
-  public GetBookingsForUserInGivenTimeRangeUsecase withMapBookings(MapBookings mapBookings) {
-    this.mapBookings = mapBookings;
-    return this;
-  }
-
-  public GetBookingsForUserInGivenTimeRangeUsecase withDateChecker(DateChecker dateChecker) {
-    this.dateChecker = dateChecker;
-    return this;
-  }
-
-  public List<RoomBookingNameAndSurnameDto> run() {
-    return getBookingForGivenUser();
-  }
-
-  private List<RoomBookingNameAndSurnameDto> getBookingForGivenUser() {
-    dateChecker.dateCheckerForSearching(bookedFrom, bookedTo);
-    userRepository.findById(userLogin).orElseThrow(() -> new BadRequestException("No such user"));
-    List<Booking> roomBookingEntityList = new ArrayList<>();
-
-    if (bookedFrom != null && bookedTo != null) {
-      roomBookingEntityList = bookingRepository.getAllBookingsWithInDateFrameAndUser(bookedFrom, bookedTo, userLogin);
+    public GetBookingsForUserInGivenTimeRangeUsecase withBookingRepository(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+        return this;
     }
-    if (bookedFrom != null && bookedTo == null) {
-      roomBookingEntityList = bookingRepository.getAllBookingsInFutureAndUser(bookedTo, userLogin);
+
+    public GetBookingsForUserInGivenTimeRangeUsecase withUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        return this;
     }
-    return mapBookings.mapBookings(roomBookingEntityList);
-  }
+
+    public GetBookingsForUserInGivenTimeRangeUsecase withBookedFrom(LocalDateTime bookedFrom) {
+        this.bookedFrom = bookedFrom;
+        return this;
+    }
+
+    public GetBookingsForUserInGivenTimeRangeUsecase withBookedTo(LocalDateTime bookedTo) {
+        this.bookedTo = bookedTo;
+        return this;
+    }
+
+    public GetBookingsForUserInGivenTimeRangeUsecase withUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+        return this;
+    }
+
+    public GetBookingsForUserInGivenTimeRangeUsecase withMapBookings(MapBookings mapBookings) {
+        this.mapBookings = mapBookings;
+        return this;
+    }
+
+    public GetBookingsForUserInGivenTimeRangeUsecase withDateChecker(DateChecker dateChecker) {
+        this.dateChecker = dateChecker;
+        return this;
+    }
+
+    public List<RoomBookingNameAndSurnameDto> run() {
+        return getBookingForGivenUser();
+    }
+
+    private List<RoomBookingNameAndSurnameDto> getBookingForGivenUser() {
+        dateChecker.dateCheckerForSearching(bookedFrom, bookedTo);
+        userRepository.findById(userLogin).orElseThrow(() -> new BadRequestException("No such user"));
+        List<Booking> roomBookingEntityList;
+        roomBookingEntityList = bookingRepository.getAllBookingsWithInDateFrameAndUser(bookedFrom, bookedTo, userLogin);
+
+        return mapBookings.mapBookings(roomBookingEntityList);
+    }
 }
