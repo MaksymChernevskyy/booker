f545e3ce66978024cb618f39982b148765388a39
admin <chernevskyymaks@gmail.com>
2020-01-29 14:52:51 +0100

    Update query in database

--- src/main/java/com/be/booker/business/repository/BookingRepository.java
+++ src/main/java/com/be/booker/business/repository/BookingRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-  @Query("select booking from Booking booking where ?1  between booking.bookedFrom and booking.bookedTo and ?2  between booking.bookedFrom and booking.bookedTo")
+  @Query("select booking from Booking booking where ?1  <= booking.bookedFrom and booking.bookedTo <= ?2 ")
   List<Booking> getAllBookingsWithIn(@Param("bookedFrom") LocalDateTime bookedFrom, @Param("bookedTo") LocalDateTime bookedTo);
 
   @Query("select booking from Booking booking where ?1  < booking.bookedFrom")
@@ -18,19 +18,19 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
   @Query("select booking from Booking booking where ?1  > booking.bookedFrom")
   List<Booking> getAllBookingsInPast(@Param("bookedFrom") LocalDateTime bookedFrom);
 
-  @Query("select booking from Booking booking where ?1  between booking.bookedFrom and booking.bookedTo and ?2  between booking.bookedFrom and booking.bookedTo and booking.roomName = ?3")
+  @Query("select booking from Booking booking where ?1  <= booking.bookedFrom and booking.bookedTo <= ?2 and booking.roomName = ?3")
   List<Booking> getAllBookingsWithInDateFrameAndRoom(@Param("bookedFrom") LocalDateTime bookedFrom, @Param("bookedTo") LocalDateTime bookedTo, @Param("roomName") String roomName);
 
   @Query("select booking from Booking booking where ?1  < booking.bookedFrom and booking.roomName = ?2 ")
   List<Booking> getAllBookingsInFutureAndRoom(@Param("bookedTo") LocalDateTime bookedTo, @Param("roomName") String roomName);
 
-  @Query("select booking from Booking booking where ?1  > booking.bookedFrom and booking.roomName = ?2 ")
+  @Query("select booking from Booking booking where ?1  < booking.bookedFrom and booking.roomName = ?2 ")
   List<Booking> getAllBookingsInPastAndRoom(@Param("bookedFrom") LocalDateTime bookedFrom, @Param("roomName") String roomName);
 
   @Query("select booking from Booking booking where  booking.roomName = ?1")
   List<Booking> getAllBookingsInRoom(@Param("roomName") String roomName);
 
-  @Query("select booking from Booking booking where  ?1  between booking.bookedFrom and booking.bookedTo and ?2  between booking.bookedFrom and booking.bookedTo and booking.userLogin = ?3")
+  @Query("select booking from Booking booking where ?1  <= booking.bookedFrom and booking.bookedTo <= ?2 and booking.userLogin = ?3")
   List<Booking> getAllBookingsWithInDateFrameAndUser(@Param("bookedFrom") LocalDateTime bookedFrom, @Param("bookedTo") LocalDateTime bookedTo, @Param("roomName") String roomName);
 
   @Query("select booking from Booking booking where  ?1  > booking.bookedFrom and booking.userLogin = ?2 ")
